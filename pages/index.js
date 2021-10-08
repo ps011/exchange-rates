56219385725a89e4d4f56373418684fc767ac1d6
lalitkushwah.dev <lalitkushwah.dev@gmail.com>
2021-10-08 17:02:38 +0530

    added searchable dropdown feature

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,46 @@
 import s from './index.module.scss';
-import { InputGroup, DropdownButton, Dropdown, FormControl, Button } from 'react-bootstrap';
+import { InputGroup, Col, FormControl, Button, Row, Container } from 'react-bootstrap';
 import { CURRENCIES } from '../constants';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Select from 'react-select';
+
+
+
 export async function getStaticProps() {
   const res = await fetch(`https://currencyapi.net/api/v1/rates?key=${process.env.API_KEY}`)
-  const rates = await res.json()
+  const rates = await res.json();
 
   return {
     props: {
-      exchangeRates: rates.rates,
+      exchangeRates: rates.rates || null,
     },
     revalidate: 3600,
   }
 }
 
 export default function Home({ exchangeRates }) {
-  const {query} = useRouter();
+  const { query } = useRouter();
+
   const [sourceCurrency, setSourceCurrency] = useState(query?.src?.toUpperCase() || /*localStorage?.getItem('src') ||*/ 'EUR');
   const [destinationCurrency, setDestinationCurrency] = useState(query?.dest?.toUpperCase() || /*localStorage?.getItem('dest') ||*/ 'INR');
   const [sourceValue, setSourceValue] = useState(1);
   const [destinationValue, setDestinationValue] = useState(0);
+  const [currencyList, setCurrencyList] = useState([]);
+
+  useEffect(() => {
+    const list = []
+    Object.keys(CURRENCIES).map(key => {
+      const obj = { label: CURRENCIES[key], value: key };
+      list.push(obj);
+    });
+    setCurrencyList(list);
+  }, [])
 
   useEffect(() => {
     calculateExchangeRate(sourceValue)
-  }, [destinationCurrency])
+  }, [destinationCurrency, sourceCurrency])
 
   const calculateExchangeRate = (e) => {
     if (sourceCurrency !== 'Currency' && destinationCurrency !== 'Currency') {
@@ -50,43 +65,79 @@ export default function Home({ exchangeRates }) {
         <h1 className={`text-center ${s.heading}`}>Currency Exchange Rates</h1>
         <div className={`mx-auto ${s.form}`}>
           <InputGroup className={`${s.inputContainer} mb-3 input-group-lg`}>
-            <DropdownButton
-              as={InputGroup.Prepend}
-              variant="primary"
-              title={sourceCurrency}
-            >
-              <div className={s.optionsContainer}>
-                {Object.keys(CURRENCIES)
-                  .map((currency, index) => (
-                    <Dropdown.Item onClick={(e) => {setSourceCurrency(currency); localStorage.setItem('src', currency)}} key={index}>
-                      {CURRENCIES[currency]}
-                    </Dropdown.Item>)
-                  )}
-              </div>
-            </DropdownButton>
-            <FormControl
-              type="number"
-              value={sourceValue}
-              onChange={(e) => calculateExchangeRate(e.target.value)} />
+            <Container>
+              <Row>
+                <Col>
+                  <label
+                    style={{
+                      color: 'white',
+                      fontWeight: 600,
+                      fontSize: 18
+                    }}>
+                    {sourceCurrency}
+                  </label>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Select
+                    hideSelectedOptions={false}
+                    isClearable={true}
+                    onChange={e => {
+                      setSourceCurrency(e.value);
+                      localStorage.setItem('src', e.value)
+                    }}
+                    options={currencyList}
+                    placeholder="Source"
+                    tabSelectsValue={false}
+                    value={sourceCurrency}
+                  />
+                </Col>
+                <Col>
+                  <FormControl
+                    type="number"
+                    value={sourceValue}
+                    onChange={(e) => calculateExchangeRate(e.target.value)} />
+                </Col>
+              </Row>
+            </Container>
           </InputGroup>
           <InputGroup className={`${s.inputContainer} mb-3 input-group-lg`}>
-            <DropdownButton
-              as={InputGroup.Prepend}
-              variant="primary"
-              title={destinationCurrency}
-            >
-              <div className={s.optionsContainer}>
-                {Object.keys(CURRENCIES)
-                  .map((currency, index) => (
-                    <Dropdown.Item onClick={(e) => {setDestinationCurrency(currency); localStorage.setItem('dest', currency)}} key={index}>
-                      {CURRENCIES[currency]}
-                    </Dropdown.Item>)
-                  )}
-              </div>
-            </DropdownButton>
-            <FormControl
-              value={destinationValue}
-              disabled />
+            <Container>
+              <Row>
+                <Col>
+                  <label
+                    style={{
+                      color: 'white',
+                      fontWeight: 600,
+                      fontSize: 18
+                    }}>
+                    {destinationCurrency}
+                  </label>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Select
+                    hideSelectedOptions={false}
+                    isClearable={true}
+                    onChange={e => {
+                      setDestinationCurrency(e.value);
+                      localStorage.setItem('dest', e.value)
+                    }}
+                    options={currencyList}
+                    placeholder="Destination"
+                    tabSelectsValue={false}
+                    value={destinationCurrency}
+                  />
+                </Col>
+                <Col>
+                  <FormControl
+                    value={destinationValue}
+                    disabled />
+                </Col>
+              </Row>
+            </Container>
           </InputGroup>
           <Button variant="dark mt-3" onClick={toggleCurrencies}>Toggle Currencies</Button>
         </div>
@@ -94,6 +145,6 @@ export default function Home({ exchangeRates }) {
           Developed and Maintained by <br /> <Link href="https://ps011.github.io">Prasheel Soni</Link>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
