bf415042de1cc041279f7b4d105de789319398ae
Prasheel Soni <prasheelsoni11@gmail.com>
2022-01-19 17:33:37 +0200

    add adsense script

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,8 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192.png"></link>
           <meta name="theme-color" content="#075dc0" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6531525250981265"
+     crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
