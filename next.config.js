f50392d811c77709d0ff8abab26edb3e9d48dbb9
Prasheel Soni <prasheel.soni@bolt.eu>
2024-01-29 00:37:18 +0200

    start using webpack 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+    future: {
+        webpack5: true,
+    },
     env: {
         API_KEY: process.env.API_KEY,
         MODE: process.env.MODE
